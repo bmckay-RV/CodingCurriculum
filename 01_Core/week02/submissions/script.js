37f56dc7bac45de467253f8404642b23d422edd7
Brandon McKay <bmckay@c02yn0x4jg5j.attlocal.net>
2020-10-17 18:15:30 -0400

    finsished week02 toy problems

---
+++ 01_Core/week02/submissions/script.js
@@ -0,0 +1,54 @@
+
+function parse() {
+    var names;
+    var sorted;
+    console.log("function call")
+    var inputVal = document.getElementById("inputtedNames").value;
+    names = inputVal.split(', ')
+    console.log(names)
+    sorted = names.sort(compare);
+    console.log(sorted)
+    inject(sorted);
+
+    
+
+
+
+}
+
+function compare(a,b){
+    var splitA = a.split(" ");
+    var splitB = b.split(" ");
+    var lastA = splitA[1];
+    var lastB = splitB[1];
+    if (lastA < lastB) return -1;
+    if (lastA > lastB) return 1;
+    return 0;
+}
+
+function inject(s) {
+    var table = document.getElementById("myTable");
+    s.forEach(e => {
+        var sortedNames = e.split(" ");
+        var row = table.insertRow(-1);
+
+        var cell1 = row.insertCell(0);
+        let text1 = document.createTextNode(sortedNames[1]);
+        console.log("adding: " + text1.textContent)
+        cell1.appendChild(text1);
+       
+       
+        var cell2 = row.insertCell(0);
+        let text2 = document.createTextNode(sortedNames[0]);
+        cell2.className = "colorMe"
+        console.log("adding: " +text2.textContent)
+        cell2.appendChild(text2);  
+    });
+    // Find a <table> element with id="myTable":
+    
+   
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    
+
+}
